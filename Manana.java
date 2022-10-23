202a30235f3df8fa3ade242fa51999cb4b934e67
Yeohjzy <116490968+Yeohjzy@users.noreply.github.com>
2022-10-23 13:05:52 +0100

    Update Manana.java

--- Manana.java
+++ Manana.java
@@ -2,5 +2,6 @@ public class Manana{
     public static void main(String[] args){
         System.out.println("hello");
         System.out.println("u Poop");
+        System.out.println("I no poop");
     }
 }
